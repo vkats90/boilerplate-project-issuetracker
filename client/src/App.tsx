15a66f85c7b9129e05a467f076f8e8d3465ede1d
vkats90 <mars77@gmail.com>
2024-10-15 22:01:05 -0600

    added frontend to issueTracker need to implement views

---
+++ client/src/App.tsx
@@ -0,0 +1,21 @@
+import './App.css'
+
+const exampleReturn = [
+  {
+    _id: '5871dda29faedc3491ff93bb',
+    issue_title: 'Fix error in posting data',
+    issue_text: 'When we post data it has an error.',
+    created_on: '2017-01-08T06:35:14.240Z',
+    updated_on: '2017-01-08T06:35:14.240Z',
+    created_by: 'Joe',
+    assigned_to: 'Joe',
+    open: true,
+    status_text: 'In QA',
+  },
+]
+
+function App() {
+  return <></>
+}
+
+export default App
